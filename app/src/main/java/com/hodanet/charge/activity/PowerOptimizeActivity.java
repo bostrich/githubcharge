96e6f68ae7d0ba60bec238bafd4c98f09d4fd296
bostrich <13057506020@163.com>
2018-04-09 19:58:59 +0800

    12

---
+++ app/src/main/java/com/hodanet/charge/activity/PowerOptimizeActivity.java
@@ -0,0 +1,15 @@
+package com.hodanet.charge.activity;
+
+
+import android.os.Bundle;
+
+import com.hodanet.charge.R;
+
+public class PowerOptimizeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power_optimize);
+    }
+}
